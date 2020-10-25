6d89191b2d11b8501152e5738c3377b33b30fe99
HamaMk <hamandishemkandabvute@gmail.com>
2020-10-25 12:05:25 +0200

    Added HomeScreen and DetailScreen components as well as navigation

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import axios from 'axios';
+
+const Stack = createStackNavigator();
+
+const HomeScreen = ({navigation}) => {
+  return(
+    <SafeAreaView style={styles.container}>
+      <Text>Home screen.</Text>
+      <StatusBar style="auto" />
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </SafeAreaView>
+  );
+}
+
+const DetailScreen = ({navigation}) => {
+  return(
+    <SafeAreaView style={styles.container}>
+      <Text>Details screen</Text>
+      <StatusBar style="auto" />
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+    </SafeAreaView>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Boom boom Pow</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'silver',
     alignItems: 'center',
     justifyContent: 'center',
   },
